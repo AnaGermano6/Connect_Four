cecba84729de6b24fd09a74216384a8faa01b473
AnaGermano6 <anasofiagermano@gmail.com>
2017-03-11 17:52:23 +0000

    update

--- src/Node.java
+++ src/Node.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class Node extends ConnectFour{
 	//tabuleiro
-	public static String matriz[][]= new String[6][7];
+	public static String[][] matriz= new String[6][7];
 	//lista dos descendentes do tabuleiro
 	public static LinkedList<Node> descendentes = new LinkedList<Node>();
 	
-	Node(String[][] m){
-		matriz=m;
+	Node(Node m){
+		Node.matriz=m.matriz;
 	}
-	
+
 	public String[][] getMatriz(){
 		return matriz;
 	}
@@ -46,13 +46,27 @@ public class Node extends ConnectFour{
 
 		for(int i=0; i<6; i++){
 			for(int j=0; j<7; j++){
-				Node filho = new Node(matriz);
-				
-				descendentes.addFirst(filho);
-				
-				
+				//enquanto o tabuleiro nao esta totalmente preenchido
+				while(m.matriz[i][j]!="X" || m.matriz[i][j]!="O"){
+					
+					//incompleto
+					Node filho = newMatriz(i, j, m);
+					descendentes.addFirst(filho);					
+				}			
 			}
 		}
 		return descendentes;
+	}
+	
+	public static Node newMatriz(int i, int j, Node m){
+		
+		//se o jogador for o X
+		if(simbolo==1)
+			m.matriz[i][j]="X";
+		//se o jogador for o O
+		else
+			Node.matriz[i][j]="O";
+		
+		return m;
 	}
 }
